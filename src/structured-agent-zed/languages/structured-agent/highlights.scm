57524990cc00c749a5d30795ba37e76b2a659197
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2026-01-22 14:09:12 +0000

    Add full syntax highlighting rules

--- src/structured-agent-zed/languages/structured-agent/highlights.scm
+++ src/structured-agent-zed/languages/structured-agent/highlights.scm
@@ -1,17 +1,90 @@
-; Keywords - using simple literal matching
+; Keywords
 "fn" @keyword
+"extern" @keyword
 "let" @keyword
+"if" @keyword
+"while" @keyword
 "return" @keyword
+"select" @keyword
+"as" @keyword
+
+; Built-in types
+"String" @type.builtin
+"Boolean" @type.builtin
+"i32" @type.builtin
+"Context" @type.builtin
+
+; Unit type
+(unit_type) @type
+
+; Function declarations
+(function_declaration
+  name: (identifier) @function)
+
+(external_function_declaration
+  name: (identifier) @function)
+
+; Function calls
+(function_call
+  function: (identifier) @function.call)
+
+; Parameters in function declarations
+(parameter
+  name: (identifier) @variable.parameter
+  type: (type) @type)
 
 ; String literals
 (string_literal) @string
 
+; Escape sequences in strings
+(escape_sequence) @string.escape
+
+; Boolean literals
+"true" @constant.builtin.boolean
+"false" @constant.builtin.boolean
+
+; Placeholder
+(placeholder) @variable.special
+
 ; Comments
 (comment) @comment
 
-; Function names
-(function_declaration
-  name: (identifier) @function)
+; Operators
+"=" @operator
+"+" @operator
+"-" @operator
+"*" @operator
+"/" @operator
+"==" @operator
+"!=" @operator
+"<" @operator
+">" @operator
+"<=" @operator
+">=" @operator
+"!" @operator
+"=>" @operator
 
-; All other identifiers
+; Punctuation
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+"," @punctuation.delimiter
+":" @punctuation.delimiter
+"->" @punctuation.delimiter
+";" @punctuation.delimiter
+
+; Variable declarations
+(let_declaration
+  name: (identifier) @variable)
+
+; Variable assignments
+(variable_assignment
+  name: (identifier) @variable)
+
+; Select bindings
+(select_clause
+  binding: (identifier) @variable)
+
+; General identifiers (catch-all for remaining identifiers)
 (identifier) @variable
