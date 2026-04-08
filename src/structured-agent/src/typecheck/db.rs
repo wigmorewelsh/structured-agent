27e219906792a50f35c6964fffb496d8083c0039
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2026-04-08 14:58:59 +0100

    Stage 3b: add SymbolTablesInput and interned key structs to db.rs

--- src/structured-agent/src/typecheck/db.rs
+++ src/structured-agent/src/typecheck/db.rs
@@ -1,5 +1,13 @@
+use super::refs::{CheckerRefs, PrimitiveRefs};
 use crate::ast::Module as AstModule;
 use crate::types::FileId;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+use std::sync::Arc;
+use structured_agent_runtime::symbols::{
+    FunctionDefinition, FunctionName, ImplDefinition, ImplKey, ModuleDefinition, ModuleName,
+    TraitDefinition, TraitName, TypeDefinition, TypeName,
+};
 
 #[salsa::db]
 pub(super) trait TypeCheckDatabase: salsa::Database {}
@@ -22,4 +30,71 @@ pub(super) struct ParsedModuleInput {
     pub(super) is_entry: bool,
     pub(super) file_id: FileId,
     pub(super) module: AstModule,
+}
+
+pub(super) struct ArcPtr<T>(Arc<T>);
+
+impl<T> ArcPtr<T> {
+    pub(super) fn new(val: T) -> Self {
+        ArcPtr(Arc::new(val))
+    }
+
+    pub(super) fn get(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T> Clone for ArcPtr<T> {
+    fn clone(&self) -> Self {
+        ArcPtr(Arc::clone(&self.0))
+    }
+}
+
+impl<T> PartialEq for ArcPtr<T> {
+    fn eq(&self, other: &Self) -> bool {
+        Arc::ptr_eq(&self.0, &other.0)
+    }
+}
+
+impl<T> Eq for ArcPtr<T> {}
+
+impl<T> Hash for ArcPtr<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        Arc::as_ptr(&self.0).hash(state);
+    }
+}
+
+#[salsa::input]
+pub(super) struct SymbolTablesInput {
+    pub(super) functions: ArcPtr<HashMap<FunctionName, Arc<FunctionDefinition<CheckerRefs>>>>,
+    pub(super) types: ArcPtr<HashMap<TypeName, Arc<TypeDefinition<CheckerRefs>>>>,
+    pub(super) traits: ArcPtr<HashMap<TraitName, Arc<TraitDefinition<CheckerRefs>>>>,
+    pub(super) impls: ArcPtr<HashMap<ImplKey, Arc<ImplDefinition<CheckerRefs>>>>,
+    pub(super) modules: ArcPtr<HashMap<ModuleName, Arc<ModuleDefinition<CheckerRefs>>>>,
+    pub(super) param_bindings: ArcPtr<HashMap<ImplKey, Arc<ImplDefinition<PrimitiveRefs>>>>,
+}
+
+#[salsa::interned]
+pub(super) struct InternedFunctionName {
+    pub(super) name: FunctionName,
+}
+
+#[salsa::interned]
+pub(super) struct InternedTypeName {
+    pub(super) name: TypeName,
+}
+
+#[salsa::interned]
+pub(super) struct InternedTraitName {
+    pub(super) name: TraitName,
+}
+
+#[salsa::interned]
+pub(super) struct InternedImplKey {
+    pub(super) key: ImplKey,
+}
+
+#[salsa::interned]
+pub(super) struct InternedModuleName {
+    pub(super) name: ModuleName,
 }
